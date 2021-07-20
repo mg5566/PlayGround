baa78971df5de516d407bc8c3c95a11b6b289769
강명구 <mg5566@hanmail.net>
2021-07-21 00:47:39 +0900

    update modulization

--- remove_directory/main.cpp
+++ remove_directory/main.cpp
@@ -5,6 +5,18 @@
 
 #include <iostream>
 
+int process_stat(std::string &file_name, struct stat *stat_buffer) {
+  if (stat(file_name.c_str(), stat_buffer) != 0) {
+    std::cerr << "fail stat(<FILE>)" << std::endl;
+    return (-1);
+  }
+  return (0);
+}
+
+void process_remove_dir(std::string file_name, DIR *dir_ptr) {
+
+}
+
 int main(void) {
   std::string file_name("./test_dir");
   struct stat stat_buffer;      // stat buffer
@@ -16,10 +28,12 @@ int main(void) {
       std::cerr << "fail stat(<File>)" << std::endl;
       return (-1);
   }
+  // process_stat(file_name, &stat_buffer);
 
   if (S_ISDIR(stat_buffer.st_mode)) {
     std::cout << file_name << " is directory" << std::endl;
     // open directory
+    // process_remove_dir(file_name, dir_ptr);
     if (!(dir_ptr = opendir(file_name.c_str()))) {
       std::cerr << "fail opendir(<FILE>)" << std::endl;
       return (-1);
@@ -32,10 +46,15 @@ int main(void) {
       // }
       std::cout << item->d_name << std::endl;
       // 이때, file 이 아닌 directory 면 재귀적으로 동작해야합니다.
+
+
+      // 하위 탐색 끝나면
+      // rmdir(file_name.c_str());
     }
     closedir(dir_ptr);
   } else if (S_ISREG(stat_buffer.st_mode)) {
     std::cout << file_name << " is file" << std::endl;
+    // remove(file_name.c_str());
   }
 
   return (0);
