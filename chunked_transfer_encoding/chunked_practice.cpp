4db90d6b21a1be734369c212708f86c45a38cdd3
강명구 <mg5566@hanmail.net>
2021-08-02 21:28:22 +0900

    add new variable append buffer

--- chunked_transfer_encoding/chunked_practice.cpp
+++ chunked_transfer_encoding/chunked_practice.cpp
@@ -6,38 +6,65 @@
 
 #define CRLF 2
 
-bool chunked_close_check(const char *buf, int len = -1) {
-  bool result;
+void set_string(std::string &str) {
+  str += "8";
+  str += "\r\n";
+  str += "fuck you";
+  str += "\r\n";
+
+  str += "b";
+  str += "\r\n";
+  str += "webserv";
+  str += "\r\n";
+  str += "GG";
+  str += "\r\n";
+
+  str += "0";
+  str += "\r\n";
+  str += "\r\n";
+}
+
+// bool chunked_close_check(const char *buf, int len = -1) {
+bool chunked_close_check(const char *buf, std::string &msg, int len = -1) {
+  bool result = false;
 
   size_t length = 0;
   char *ptr;
 
   ptr = (char *)buf;
-  if (len == -1)
-    length = strtoul(ptr, &ptr, 16) + CRLF;
-  else
+  if (len == -1) {
+    std::cout << "------------fbuffer---------------" << std::endl;
+    length = strtoul(ptr, &ptr, 16);
+    std::cout << length << " : " << ptr << std::endl;
+    length += CRLF;
+    msg.append(ptr, length);
+    std::cout << "------------fbuffer---------------" << std::endl;
+    while ((length = strtoul(ptr + length, &ptr, 16)) > 0) {
+      std::cout << "------------fbuffer---------------" << std::endl;
+      std::cout << length << " : " << ptr << std::endl;
+      length += CRLF;
+      // append
+      msg.append(ptr, length);
+      // check append length
+      // next append
+      // check append length
+      // loop at finish
+      std::cout << "------------fbuffer---------------" << std::endl;
+      // if (strcmp(ptr, "\r\n\r\n") == 0)
+      //   return (true);
+    }
+    if (length == 0) {
+      if (strcmp(ptr, "\r\n\r\n") == 0) {
+        std::cout << "===finish===" << std::endl;
+        return (true);
+      } else {
+        std::cout << "===can't finish===" << std::endl;
+        return (false);
+      }
+    }
+  } else {
     length = len;
-
-  while ((length = strtoul(ptr + length + CRLF, &ptr, 16)) > 0) {
-    std::cout << "------------fbuffer---------------" << std::endl;
-    std::cout << ptr << std::endl;
-    std::cout << "------------fbuffer---------------" << std::endl;
-    // if (strcmp(ptr, "\r\n\r\n") == 0)
-    //   return (true);
   }
-  if (length == 0) {
-    if (strcmp(ptr, "\r\n\r\n") == 0) {
-      std::cout << "===finish===" << std::endl;
-      return (true);
-    }
-    else {
-      std::cout << "===can't finish===" << std::endl;
-      return (false);
-    }
-  }
-
-  result = false;
-
   return (result);
 }
 
@@ -47,6 +74,14 @@ int main(int argc, char **argv) {
     std::string str((std::istreambuf_iterator<char>(t)),
                     std::istreambuf_iterator<char>());
 
+    // int main(void) {
+    //   if (1) {
+    //     std::string str;
+
+    //     // set str
+    // set_string(str);
+    std::string msg;
+
     std::cout << "=============file===============" << std::endl;
     std::cout << str << std::endl;
     std::cout << "=============file===============" << std::endl;
@@ -54,10 +89,15 @@ int main(int argc, char **argv) {
     std::cout << "\n\nlet's check\n\n"
               << std::endl;
 
-    if (chunked_close_check(str.c_str()))
+    // if (chunked_close_check(str.c_str()))
+    if (chunked_close_check(str.c_str(), msg))
       std::cout << "close" << std::endl;
     else
       std::cout << "can't close" << std::endl;
+
+    std::cout << "=============msg================" << std::endl;
+    std::cout << msg << std::endl;
+    std::cout << "=============msg================" << std::endl;
 
   } else
     std::cout << "Usage: ./chunked_practice filepath" << std::endl;
