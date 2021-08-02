9ac14063350b0f97823367ea119799a5d9183a02
강명구 <mg5566@hanmail.net>
2021-08-02 22:49:02 +0900

    update new logic

--- chunked_transfer_encoding/chunked_practice.cpp
+++ chunked_transfer_encoding/chunked_practice.cpp
@@ -33,37 +33,44 @@ bool chunked_close_check(const char *buf, std::string &msg, int len = -1) {
 
   ptr = (char *)buf;
   if (len == -1) {
-    std::cout << "------------fbuffer---------------" << std::endl;
     length = strtoul(ptr, &ptr, 16);
-    std::cout << length << " : " << ptr << std::endl;
+    // std::cout << length << " : " << ptr << std::endl;
     length += CRLF;
     msg.append(ptr, length);
-    std::cout << "------------fbuffer---------------" << std::endl;
     while ((length = strtoul(ptr + length, &ptr, 16)) > 0) {
-      std::cout << "------------fbuffer---------------" << std::endl;
-      std::cout << length << " : " << ptr << std::endl;
+      // std::cout << length << " : " << ptr << std::endl;
       length += CRLF;
       // append
       msg.append(ptr, length);
       // check append length
+      // append 된 character 의 수를 알아야합니다. appended_len
+      // appended_len 이 length 와 같아야 정상이지만, 메세지가 그렇게 호락호락하게 오질 않습니다.
+      // 따라서 appeded_len 이 length 보다 작으면 다음 조건문을 실행합니다.
+      /*
+      if (appended_len < length) {
+        // 해당 부분은 수정이 필요합니다.
+        // 다음 메세지를 기다려야합니다.
+        // 다음 메세지에는 length 부터 오는 것이 아닌 append 되어야할 남은 msg 가 먼저옵니다.
+        // 남은 msg 를 붙이고 다음 동작을 합니다.
+        msg.append(ptr, length - appended_len);
+        // length - appended_len 을 기억하고 있다가 다음 메시지가 왔을때, 해당 함수가 호출되면 append msg 부터 하도록 해야할듯?!
+        continue;
+      } else if (appened_len > length) { // appended_len 이 length 보다 클 순 없습니다. 그런경우 false 를 반환합니다.
+        return (false);
+      }
+      */
       // next append
       // check append length
-      // loop at finish
-      std::cout << "------------fbuffer---------------" << std::endl;
-      // if (strcmp(ptr, "\r\n\r\n") == 0)
-      //   return (true);
     }
+    // finish loop block
     if (length == 0) {
       if (strcmp(ptr, "\r\n\r\n") == 0) {
         std::cout << "===finish===" << std::endl;
-        return (true);
+        result = true;
       } else {
         std::cout << "===can't finish===" << std::endl;
-        return (false);
       }
     }
-  } else {
-    length = len;
   }
   return (result);
 }
@@ -74,20 +81,19 @@ int main(int argc, char **argv) {
     std::string str((std::istreambuf_iterator<char>(t)),
                     std::istreambuf_iterator<char>());
 
-    // int main(void) {
-    //   if (1) {
-    //     std::string str;
+// int main(void) {
+//   if (1) {
+//     std::string str;
 
-    //     // set str
-    // set_string(str);
+//     // set str
+// set_string(str);
     std::string msg;
 
     std::cout << "=============file===============" << std::endl;
     std::cout << str << std::endl;
     std::cout << "=============file===============" << std::endl;
 
-    std::cout << "\n\nlet's check\n\n"
-              << std::endl;
+    std::cout << "\n\nlet's check\n\n" << std::endl;
 
     // if (chunked_close_check(str.c_str()))
     if (chunked_close_check(str.c_str(), msg))
