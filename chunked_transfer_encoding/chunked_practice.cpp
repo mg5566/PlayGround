6de8d0c7d67f9a88a2fe7b9f13afdc4863196b06
강명구 <mg5566@hanmail.net>
2021-08-03 14:12:04 +0900

    add data struct

--- chunked_transfer_encoding/chunked_practice.cpp
+++ chunked_transfer_encoding/chunked_practice.cpp
@@ -6,6 +6,12 @@
 
 #define CRLF 2
 
+typedef struct  s_data {
+  size_t        appended_length;
+  std::string   message_buffer;
+  s_data data() { appended_length = 0; };  // 생성과 동시에 초기화를 위한 생성자
+}               t_data;
+
 void set_string(std::string &str) {
   str += "8";
   str += "\r\n";
@@ -25,7 +31,8 @@ void set_string(std::string &str) {
 }
 
 // bool chunked_close_check(const char *buf, int len = -1) {
-bool chunked_close_check(const char *buf, std::string &msg, int len = -1) {
+// bool chunked_close_check(const char *buf, std::string &msg, int len = -1) {
+bool chunked_close_check(const char *buf, t_data &data, int len = -1) {
   bool result = false;
 
   size_t length = 0;
@@ -43,8 +50,8 @@ bool chunked_close_check(const char *buf, std::string &msg, int len = -1) {
     while ((length = strtoul(ptr + length, &ptr, 16)) > 0) {
       std::cout << length << " : " << ptr << std::endl;
       // append
-      msg.append(ptr + CRLF, length);
-      std::cout << "msg test : " << msg << std::endl;
+      data.message_buffer.append(ptr + CRLF, length);  // 중요한 문제 여기서 appended_len 을 어떻게 계산을 할 수 있는가!?
+      std::cout << "data.message_buffer test : " << data.message_buffer << std::endl;
       length += CRLF;
       // check append length
       // append 된 character 의 수를 알아야합니다. appended_len
@@ -56,7 +63,7 @@ bool chunked_close_check(const char *buf, std::string &msg, int len = -1) {
         // 다음 메세지를 기다려야합니다.
         // 다음 메세지에는 length 부터 오는 것이 아닌 append 되어야할 남은 msg 가 먼저옵니다.
         // 남은 msg 를 붙이고 다음 동작을 합니다.
-        msg.append(ptr, length - appended_len);
+        data.message_buffer.append(ptr, length - appended_len);
         // length - appended_len 을 기억하고 있다가 다음 메시지가 왔을때, 해당 함수가 호출되면 append msg 부터 하도록 해야할듯?!
         continue;
       } else if (appened_len > length) { // appended_len 이 length 보다 클 순 없습니다. 그런경우 false 를 반환합니다.
@@ -85,21 +92,25 @@ int main(int argc, char **argv) {
     std::string str((std::istreambuf_iterator<char>(t)),
                     std::istreambuf_iterator<char>());
 
-// int main(void) {
-//   if (1) {
-//     std::string str;
-//     // set str
-// set_string(str);
-    std::string msg;  // buffer, decoded data
+    // int main(void) {
+    //   if (1) {
+    //     std::string str;
+    //     // set str
+    // set_string(str);
+    // std::string msg;  // buffer, decoded data
+    t_data  data;  // 선언과 동시에 생성자가 알아서 호출됨.
+    // t_data *data = (t_data *)malloc(sizeof(t_data) * 1)); 이 과정을 생략할 수 있습니다.
 
     std::cout << "=============file===============" << std::endl;
     std::cout << str << std::endl;  // encoded data
     std::cout << "=============file===============" << std::endl;
 
-    std::cout << "\n\nlet's check\n\n" << std::endl;
+    std::cout << "\n\nlet's check\n\n"
+              << std::endl;
 
     // if (chunked_close_check(str.c_str()))
-    if (chunked_close_check(str.c_str(), msg))
+    // if (chunked_close_check(str.c_str(), msg))
+    if (chunked_close_check(str.c_str(), data))
       std::cout << "close" << std::endl;
     else
       std::cout << "can't close" << std::endl;
