fcd6e5305386cb82eeb560bb398cbfbc622d72ab
강명구 <mg5566@hanmail.net>
2021-09-10 00:39:03 +0900

    add Component

--- React_practice/blog/src/App.js
+++ React_practice/blog/src/App.js
@@ -14,12 +14,22 @@ function App() {
   function print_num(num) {
 	  return (num);
   }
+
+  function change_subject_func() {
+	var newArr = [...subject];
+	newArr[0] = "여자 코트 추천";
+	// newArr.sort();  오 이런것도 가능하다니 ㅎㅎ
+	change_subject(newArr);
+  }
+
   return (
     <div className="App">
       <div className="black-nav">
         <div>지속가능한 개발 Blog</div>
       </div>
-	  <button onClick={ ()=>{ change_subject(['여자 코트 추천', '강남 우동 맛집', '강남 고기 맛집'])} }>state 변경 버튼</button>
+	  {/* <button onClick={ ()=>{ change_subject(['여자 코트 추천', '강남 우동 맛집', '강남 고기 맛집'])} }>state 변경 버튼</button> */}
+	  {/* change_subject_func() 하면 바로 호출되기 때문에 ()는 제거하세요 */}
+	  <button onClick={ change_subject_func }>state 변경 버튼</button>
 
 	  {/* <h4 style={ custom_style }>블로그 글 { posts }</h4> */}
 	  {/* <h4 style={ { color : 'blue', fontSize : '30px' } }>블로그 글 { posts }</h4> */}
@@ -42,8 +52,28 @@ function App() {
 		<p>2021.09.09 목요일 발행</p>
 		<hr/>
 	  </div>
+
+	  {/* model view */}
+	  {/* <div className="model">
+		  <h2>Subject</h2>
+		  <p>Date</p>
+		  <p>Body</p>
+	  </div> */}
+	  <Model></Model>
+
     </div>
   );
+}
+
+// Component
+function Model() {
+	return (
+	<div className="model">
+		<h2>Subject</h2>
+		<p>Date</p>
+		<p>Body</p>
+	</div>
+	);
 }
 
 export default App;
