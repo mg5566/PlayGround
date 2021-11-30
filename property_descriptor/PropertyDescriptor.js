8a9bfd65484f2308baa03ac8f9f4302d2607d50d
강명구 <mg5566@hanmail.net>
2021-11-30 15:30:57 +0900

    feat: add accessor property

--- property_descriptor/PropertyDescriptor.js
+++ property_descriptor/PropertyDescriptor.js
@@ -1,10 +1,28 @@
 const person = {
+  // data property
   name: 'Kang',
+  age: 29,
+
+  // accessor property
+  get info() {
+    return `${this.name}, age: ${this.age}`
+  },
+
+  set info(name) {
+    this.name = name;
+  },
 };
 
-person.age = 29;
+// person.age = 29;
 
 console.log(Object.getOwnPropertyDescriptor(person, 'age'));
 console.log(Object.getOwnPropertyDescriptor(person, 'name'));
 
+console.log(Object.getOwnPropertyDescriptors(person));
+
+console.log('info', person.info);
+
+person.info = 'Ka';
+
+console.log('new info', person.info);
 console.log(Object.getOwnPropertyDescriptors(person));
