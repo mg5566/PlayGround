312d3e0fcadb070c7a7edc057b53584f64b9bec2
강명구 <mg5566@hanmail.net>
2022-03-24 17:15:56 +0900

    feat: add Modal on SignIn component

--- practice-mui/src/templates/sign-in/SignIn.tsx
+++ practice-mui/src/templates/sign-in/SignIn.tsx
@@ -12,8 +12,22 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Modal } from "@mui/material";
+import { useState } from "react";
 
 const theme = createTheme();
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function SignIn() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -23,7 +37,12 @@ export default function SignIn() {
       email: data.get("email"),
       password: data.get("password"),
     });
+    handleOpen();
   };
+  const [open, setOpen] = useState<boolean>(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
 
   return (
     <ThemeProvider theme={theme}>
@@ -81,6 +100,21 @@ export default function SignIn() {
             >
               Sign In
             </Button>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Sign In
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  This is Demo Modal
+                </Typography>
+              </Box>
+            </Modal>
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2">
