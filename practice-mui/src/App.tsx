d86679632ce62f261ac6066df299c39e5910c554
강명구 <mg5566@hanmail.net>
2022-04-01 12:53:34 +0900

    temporary commit

--- practice-mui/src/App.tsx
+++ practice-mui/src/App.tsx
@@ -1,9 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Dashboard from "./templates/dashboard/Dashboard";
 import SignIn from "./templates/sign-in/SignIn";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loginToken = localStorage.getItem("loginToken");
+    if (loginToken === null) {
+      navigate("/dashboard");
+    }
+    navigate("/signin");
+  }, [navigate]);
+
   return (
     <div>
       <Routes>
