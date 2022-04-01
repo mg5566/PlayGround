054ff74e8309ee3fdf7f365e18a05338d90360dd
강명구 <mg5566@hanmail.net>
2022-04-01 15:36:01 +0900

    feat: create new charts

---
+++ echart_react/src/App.js
@@ -0,0 +1,9 @@
+import Example from "./echarts/Example";
+
+function App() {
+  return <div>
+    <Example />
+  </div>;
+}
+
+export default App;
---
+++ echart_react/src/echarts/Example.js
@@ -0,0 +1,69 @@
+import ReactECharts from "echarts-for-react";
+
+const Example = () => {
+  const barChartOptions = {
+    title: {
+      text: 'Basic fuck you',
+      subtext: 'Fake Data',
+      left: 'center'
+    },
+    xAxis: {
+      type: "category",
+      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: "line",
+        smooth: true,
+      },
+    ],
+    tooltip: {
+      trigger: "axis",
+    },
+  };
+
+  const pieChartOptions = {
+    title: {
+      text: 'Referer of a Website',
+      subtext: 'Fake Data',
+      left: 'center'
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'vertical',
+      left: 'left'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: '50%',
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  };
+
+  return <ReactECharts option={barChartOptions} />;
+  // return <ReactECharts option={pieChartOptions} />;
+};
+
+export default Example;
